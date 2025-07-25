a341f18373e31de47268d9010be7057674f5ec4a
GoodbyeMyself <15500409521@163.com>
2025-07-25 17:53:01 +0800

    tailwind  配置更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+import commonConfig from './tailwind-common-config'
+
+const config = {
   content: [
     './src/pages/**/*.tsx',
     './src/components/**/*.tsx',
     './src/layouts/**/*.tsx',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/context/**/*.{js,ts,jsx,tsx}',
   ],
+  ...commonConfig,
 }
+
+export default config
+
