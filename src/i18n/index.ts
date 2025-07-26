66b344a105d3f00d5019fb0440c33767f79096ea
GoodbyeMyself <15500409521@163.com>
2025-07-26 15:30:45 +0800

    去掉一个无用的输出

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -14,7 +14,6 @@ export type Locale = typeof i18n['locales'][number]
 export const setLocaleOnClient = (locale: Locale, reloadPage = true) => {
     Cookies.set(LOCALE_COOKIE_NAME, locale, { expires: 365 })
     changeLanguage(locale)
-    console.log(2222, '<- 打印 xxx');
     if (reloadPage) {
         location.reload()
     }
